f4ad9cd6528448ea113ab768f60eb9cdf7bc3c21
Harshit Goyal <goyalharshit029@gmail.com>
2025-08-31 19:32:21 +0530

    Project Completed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 const Dashboard = () => {
     const navigate = useNavigate()
+    const { companyData, setCompanyData, setCompanyToken } = useContext(AppContext)
+    // Function to logout for company
+    const logout = () => {
+        setCompanyToken(null)
+        localStorage.removeItem('companyToken')
+        setCompanyData(null)
+        navigate('/')
+    }
+    useEffect(() => {
+        if (companyData) {
+            navigate('/dashboard/manage-job')
+        }
+    }, [companyData])
     return (
         <div className='min-h-screen'>
             {/* Navbar for Recruiter Panel */}
             <div className='shadow py-4'>
                 <div className='px-5 flex justify-between items-center'>
                     <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
-                    <div className='flex items-center gap-3'>
-                        <p className='max-sm:hidden'>Welcome, Harshit</p>
-                        <div className='relative group'>
-                            <img className='w-8 border  rounded-full' src={assets.company_icon} alt="" />
-                            <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
-                                <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                                    <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
-                                </ul>
+                    {companyData && (
+                        <div className='flex items-center gap-3'>
+                            <p className='max-sm:hidden'>Welcome, {companyData.name}</p>
+                            <div className='relative group'>
+                                <img className='w-8 border  rounded-full' src={companyData.image} alt="" />
+                                <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                                    <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                                        <li onClick={logout} className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
-            <div className='flex irems-start'>
+            <div className='flex items-start'>
                 {/* Left Sidebar with option to add job, manage jobs, view applications */}
                 <div className='inline-block min-h-screen border-r-2'>
                     <ul className='flex flex-col items-start pt-5 text-gray-800'>
@@ -40,7 +56,7 @@ const Dashboard = () => {
                         </NavLink>
                     </ul>
                 </div>
-                <div>
+                <div className='flex-1 h-full p-2 sm:p-5'>
                     <Outlet />
                 </div>
             </div>
--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { manageJobsData } from '../assets/assets'
 import moment from 'moment'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import Loading from '../components/Loading'
 const ManageJobs = () => {
     const navigate = useNavigate()
-    return (
+    const [jobs, setJobs] = useState(false)
+    const { backendUrl, companyToken } = useContext(AppContext)
+    // Function to fetch company Job Applications data
+    const fetchCompanyJobs = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/company/list-jobs',
+                { headers: { token: companyToken } }
+            )
+            if (data.success) {
+                setJobs(data.jobs.reverse())
+                console.log(data.jobs);
+
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+    // Function to change Job Visibility
+    const changeJobVisibility = async (jobId) => {
+        try {
+            const { data } = await axios.post(backendUrl + '/api/company/change-visibility',
+                { id: jobId },
+                { headers: { token: companyToken } }
+            )
+            if (data.success) {
+                toast.success(data.message)
+                fetchCompanyJobs()
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+    useEffect(() => {
+        if (companyToken) {
+            fetchCompanyJobs()
+        }
+    }, [companyToken])
+    return jobs ? jobs.length === 0 ? (
+        <div className='flex items-center justify-center h-[70vh]'>
+            <p className='text-xl sm:text-2xl'>No Jobs Available or posted</p>
+        </div>
+    ) : (
         <div className='container p-4 max-w-5xl'>
             <div className='overflow-x-auto'>
                 <table className='min-w-full bg-white  border-gray-200 max-sm:text-sm'>
@@ -19,15 +68,15 @@ const ManageJobs = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {manageJobsData.map((job, index) => (
+                        {jobs.map((job, index) => (
                             <tr key={index} className='text-gray-700'>
                                 <td className='py-2 px-4 border-b max-sm:hidden'>{index + 1}</td>
                                 <td className='py-2 px-4 border-b'>{job.title}</td>
                                 <td className='py-2 px-4 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
-                                <td className='py-2 px-4 border-b max-sm:hidden'>{job.location}</td>
+                                <td className='py-2 px-4 border-b'>{job.location}</td>
                                 <td className='py-2 px-4 border-b text-center'>{job.applicants}</td>
                                 <td className='py-2 px-4 border-b'>
-                                    <input className='scale-125 ml-4' type="checkbox" />
+                                    <input onChange={() => changeJobVisibility(job._id)} className='scale-125 ml-4' type="checkbox" checked={job.visible} />
                                 </td>
                             </tr>
                         ))}
@@ -38,6 +87,6 @@ const ManageJobs = () => {
                 <button onClick={() => navigate('/dashboard/add-job')} className='bg-black text-white py-2 px-4 rounded'>Add new job</button>
             </div>
         </div>
-    )
+    ) : <Loading />
 }
 export default ManageJobs
