c6278f0522af4a46aa127754a9c00569992d8973
Harshit Goyal <goyalharshit029@gmail.com>
2025-09-10 00:28:51 +0530

    Rectification

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -80,7 +80,7 @@ const Applications = () => {
                         {userApplications.map((job, index) => true ? (
                             <tr key={index}>
                                 <td className='py-3 px-4 flex items-center gap-2 border-b'>
-                                    <img className='w-8 h-8' src={job.companyId.image} alt="" />
+                                    <img className='w-8 h-8' src={job.companyId?.image} alt="" />
                                     {job.companyId.name}
                                 </td>
                                 <td className='py-2 px-4 border-b'>{job.jobId?.title}</td>
