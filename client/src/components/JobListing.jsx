1bcdb67d5086294b3270ce41ca88ee513a56cebe
Harshit Goyal <goyalharshit029@gmail.com>
2025-09-18 23:56:04 +0530

    f

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -22,7 +22,7 @@ const JobListing = () => {
     useEffect(() => {
         const matchesCategory = job => selectedCategories.length === 0 || selectedCategories.includes(job.category)
         const matchesLocation = job => selectedLocations.length === 0 || selectedLocations.includes(job.location)
-        const matchesTitle = job => searchFilter.title === "" || job.title.toLowerCase().includes(searchFilter.title.toLowerCase())
+        const matchesTitle = job => searchFilter.title === "" || job?.title?.toLowerCase().includes(searchFilter.title.toLowerCase())
         const matchesSearchLocation = job => searchFilter.location === "" || job.location.toLowerCase().includes(searchFilter.location.toLowerCase())
         const newFilteredJobs = jobs.slice().reverse().filter(
             job => matchesCategory(job) && matchesLocation(job) && matchesTitle(job) && matchesSearchLocation(job)
