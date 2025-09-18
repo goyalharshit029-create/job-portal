3cee9cadb0133ad16980f5f432455c4826be5462
Harshit Goyal <goyalharshit029@gmail.com>
2025-09-19 00:00:22 +0530

    g

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -13,7 +13,7 @@ const JobCard = ({ job }) => {
                 <span className='bg-blue-100 border-blue-200 px-4 py-1.5 rounded'>{job?.location}</span>
                 <span className='bg-red-100 border-red-200 px-4 py-1.5 rounded'>{job?.level}</span>
             </div>
-            <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{ __html: job?.description.slice(0, 150) }}></p>
+            <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{ __html: job?.description?.slice(0, 150) }}></p>
             <div className='mt-4 flex gap-4 text-sm'>
                 <button onClick={() => { job?._id && navigate(`/apply-job/${job._id}`); scrollTo(0, 0) }} className='bg-blue-600 text-white px-4 py-2 rounded'>Apply now</button>
                 <button onClick={() => { job?._id && navigate(`/apply-job/${job._id}`); scrollTo(0, 0) }} className='text-gray-500 border border-gray-500 rounded px-4 py-2'>Learn more</button>
