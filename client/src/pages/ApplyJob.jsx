b8545f2d072e0790c116f49587a2403c39a0a995
Harshit Goyal <goyalharshit029@gmail.com>
2025-09-19 00:17:50 +0530

    h

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -82,36 +82,36 @@ const ApplyJob = () => {
                         <div className='flex flex-col md:flex-row items-center'>
                             <img className='h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border' src={JobData?.companyId?.image} alt="" />
                             <div className='text-center md:text-left text-neutral-700'>
-                                <h1 className='text-2xl sm:text-4xl font-medium'>{JobData.title}</h1>
+                                <h1 className='text-2xl sm:text-4xl font-medium'>{JobData?.title}</h1>
                                 <div className='flex flex-row flex-wrap max-md:justify-center gap-y-2 gap-6 items-center text-gray-600 mt-2'>
                                     <span className='flex items-center gap-1'>
                                         <img src={assets.suitcase_icon} alt="" />
-                                        {JobData.companyId.name}
+                                        {JobData?.companyId?.name}
                                     </span>
                                     <span className='flex items-center gap-1'>
                                         <img src={assets.location_icon} alt="" />
-                                        {JobData.location}
+                                        {JobData?.location}
                                     </span>
                                     <span className='flex items-center gap-1'>
                                         <img src={assets.person_icon} alt="" />
-                                        {JobData.level}
+                                        {JobData?.level}
                                     </span>
                                     <span className='flex items-center gap-1'>
                                         <img src={assets.money_icon} alt="" />
-                                        CTC: {kconvert.convertTo(JobData.salary)}
+                                        CTC: {kconvert.convertTo(JobData?.salary)}
                                     </span>
                                 </div>
                             </div>
                         </div>
                         <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
                             <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded'>{isAlreadyApplied ? 'Already Applied' : 'Apply Now'}</button>
-                            <p className='mt-1 text-gray-600'>Posted {moment(JobData.date).fromNow()}</p>
+                            <p className='mt-1 text-gray-600'>Posted {JobData?.date ? moment(JobData.date).fromNow() : ''}</p>
                         </div>
                     </div>
                     <div className='flex flex-col lg:flex-row justify-between items-start'>
                         <div className='w-full lg:w-2/3'>
                             <h2 className='font-bold text-2xl mb-4'>Job description</h2>
-                            <div className='rich-text' dangerouslySetInnerHTML={{ __html: JobData.description }}></div>
+                            <div className='rich-text' dangerouslySetInnerHTML={{ __html: JobData?.description || '' }}></div>
                             <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>{isAlreadyApplied ? 'Already Applied' : 'Apply Now'}</button>
                         </div>
                         {/* Right Section More Jobs */}
