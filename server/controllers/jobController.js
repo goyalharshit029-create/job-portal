c58d2ff7e7652ab5ebc76ec28d9cd5b2d2d4642d
Harshit Goyal <goyalharshit029@gmail.com>
2025-09-18 18:34:07 +0530

    no cgange

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -16,7 +16,7 @@ export const getJobById = async (req, res) => {
         const job = await Job.findById(id)
             .populate({
                 path: 'companyId',
-                select: 'name image'
+                select: '-password'
             })
         if (!job) {
             return res.json({
